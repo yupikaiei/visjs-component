2810ead527a2d36f9a6b88e5cb5ffbe4f12054d2
Magno Sousa <magno.sousa@dllgroup.com>
2023-08-17 00:21:32 +0200

    added support for multiple visualizations

--- src/visjs_component/__init__.py
+++ src/visjs_component/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Callable
+import json
 
 import streamlit as st
 import streamlit.components.v1 as components
@@ -8,87 +9,84 @@ import streamlit.components.v1 as components
 # and that the code to display that component is in the "frontend" folder
 frontend_dir = (Path(__file__).parent / "frontend").absolute()
 _component_func = components.declare_component(
-	"visjs_component", path=str(frontend_dir)
+    "visjs_component", path=str(frontend_dir)
 )
-
-selected_node = None
-all_nodes = None
-
-def find_selected_node():
-    global selected_node
-    global all_nodes
-
-    for node in all_nodes:
-        if node["id"] == selected_node:
-            return node
-
-def selected_node_details():
-    global selected_node
-    global all_nodes
-
-    if selected_node is None:
-        return 
-    
-    node = find_selected_node()
-    title = node["label"]
-    description = node["description"]
-    noveltyScore = node["noveltyScore"]
-    marketScore = node["marketScore"]
-    usefulnessScore = node["usefulnessScore"]
-    easeOfImplementationScore = node["easeOfImplementationScore"]
-    impactScore = node["impactScore"]
-    createdAt = node["createdAt"]
-
-    st.write(f"## {title}")
-    # st.write(f"**Title**: {title}")
-    st.write(f"**Description**: {description}")
-    st.write(f"**Novelty Score**: {noveltyScore}")
-    st.write(f"**Market Score**: {marketScore}")
-    st.write(f"**Usefulness Score**: {usefulnessScore}")
-    st.write(f"**Ease of Implementation Score**: {easeOfImplementationScore}")
-    st.write(f"**Impact Score**: {impactScore}")
-    st.write(f"**Created At**: {createdAt}")
-
 
 # Create the python function that will be called
 def visjs_component(
-    nodes: [list] = [],
-    edges: [list] = [],
-    options: dict = {},
+    type: [str] = "network",
+    data: dict = {},
+    eventNames: list = [],
     key: Optional[str] = None,
 ):
-    global selected_node
-    global all_nodes
-
-    all_nodes = nodes
     """
     Add a descriptive docstring
     """
     component_value = _component_func(
-        nodes=nodes,
-        edges=edges,
-        options=options,
+        type=type,
+        data=data,
+        eventNames=eventNames,
         key=key,
     )
 
-    selected_node = component_value
-
-    # st.write(component_value)
+    # print(component_value)
 
     return component_value
 
-# def main(
-#     nodes: [list] = [],
-#     edges: [list] = [],
-#     options: dict = {},
-# ):
-#     st.set_page_config(layout="wide")
 
-#     st.write("## Data Vis")
-#     # value = visjs_component(nodes=nodes, edges=edges, options=options)
+def visjs(
+    title: [str] = "Data Vis",
+    type: [str] = "network",
+    data: [dict] = {},
+    eventHandlers: [list] = [],
+):
+    st.set_page_config(layout="wide")
+    st.write(title)
 
-#     # st.write(value)
+    eventNames = []
+    for eventHandler in eventHandlers:
+        eventNames.append(eventHandler["event"])
+
+    value = visjs_component(type=type, data=data, eventNames=eventNames)
+
+    if eventHandlers:
+        if value is not None:
+            eventData = json.loads(value)
+
+            for eventHandler in eventHandlers:
+                if eventHandler["event"] == eventData['event']:
+                    eventHandler["callback"](eventData)
 
 
 # if __name__ == "__main__":
-#     main()
+#     main(title="# Data Vis", type="network", data={
+#         "nodes": [
+#             {"id": 1, "label": 'Node 1'},
+#             {"id": 2, "label": 'Node 2'},
+#             {"id": 3, "label": 'Node 3'},
+#             {"id": 4, "label": 'Node 4'},
+#             {"id": 5, "label": 'Node 5'}
+#         ],
+#         "edges": [
+#             {"from": 1, "to": 3},
+#             {"from": 1, "to": 2},
+#             {"from": 2, "to": 4},
+#             {"from": 2, "to": 5}
+#         ],
+#         "options": {
+#             "nodes": {
+#                 "shape": "dot",
+#                 "size": 16,
+#             },
+#             "edges": {
+#                 "color": "#000000",
+#             },
+#             "physics": {
+#                 "enabled": True,
+#             },
+#             "interaction": {
+#                 "hover": True,
+#             },
+#             "height": "500px",
+#         }
+#     })
