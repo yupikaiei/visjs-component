24abea6a5a63036e0bf16734e41f32e32e95357b
Magno Sousa <magno.sousa@dllgroup.com>
2023-08-17 00:21:02 +0200

    added support for multiple visualizations

--- src/visjs_component/frontend/main.js
+++ src/visjs_component/frontend/main.js
@@ -2,6 +2,93 @@
 // `streamlit-component-lib.js`.
 // If you get an error about "Streamlit" not being defined, that
 // means you're missing that file.
+const TYPE_NETWORK = "network"
+const TYPE_TIMELINE = "timeline"
+const TYPE_GRAPH3d = "graph3d"
+
+function formatedEvent(type, event, data) {
+  return sendValue(JSON.stringify({
+    type: type,
+    event: event,
+    data: data
+  }))
+}
+
+function renderGraph3d(data, eventNames) {
+  let graphData = data.data
+  let options = data.options
+
+  var graphDataset = new vis.DataSet();
+
+  graphData.forEach(function (item) {
+    graphDataset.add(item);
+  });
+
+  var container = document.getElementById('my_render');
+  var graph3d = new vis.Graph3d(container, graphDataset, options);
+
+  eventNames = eventNames || []
+
+  eventNames.forEach(event => {
+    graph3d.on(event, (params) => formatedEvent(TYPE_GRAPH3d, event, params))
+  })
+}
+
+function renderTimeline(data, eventNames) {
+  let items = new vis.DataSet(data.items);
+  let groups = data.groups
+  let options = data.options
+
+  // DOM element where the Timeline will be attached
+  var container = document.getElementById('my_render');
+
+  // Create a Timeline
+  if (groups) {
+    var timeline = new vis.Timeline(container, items, groups, options);
+  } else {
+    var timeline = new vis.Timeline(container, items, options);
+  }
+
+  eventNames = eventNames || []
+
+  eventNames.forEach(event => {
+    timeline.on(event, (params) => formatedEvent(TYPE_TIMELINE, event, params))
+  })
+}
+
+
+function renderNetwork(data, eventNames) {
+  let nodes = data.nodes
+  let edges = data.edges
+  let options = data.options
+
+  // create an array with nodes
+  var vis_nodes = new vis.DataSet(nodes);
+
+  // create an array with edges
+  var vis_edges = new vis.DataSet(edges);
+
+  // create a network
+  var container = document.getElementById('my_render');
+
+  // provide the data in the vis format
+  var data = {
+    nodes: vis_nodes,
+    edges: vis_edges
+  };
+  var vis_options = options;
+
+  // initialize your network!
+  var network = new vis.Network(container, data, options);
+
+  eventNames = eventNames || []
+
+  eventNames.forEach(event => {
+    network.on(event, (params) => formatedEvent(TYPE_NETWORK, event, params))
+  })
+
+}
+
 
 function sendValue(value) {
   Streamlit.setComponentValue(value)
@@ -18,40 +105,15 @@ function onRender(event) {
     // You most likely want to get the data passed in like this
     // const {input1, input2, input3} = event.detail.args
 
-    const { nodes, edges, options } = event.detail.args;
-    console.log(nodes)
-    console.log(edges)
-    console.log(options)
+    const { type, data, eventNames } = event.detail.args;
 
-    // create an array with nodes
-    var vis_nodes = new vis.DataSet(nodes);
-
-    // create an array with edges
-    var vis_edges = new vis.DataSet(edges);
-
-    // create a network
-    var container = document.getElementById('mynetwork');
-
-    // provide the data in the vis format
-    var data = {
-        nodes: vis_nodes,
-        edges: vis_edges
-    };
-    var vis_options = options;
-
-    // initialize your network!
-    var network = new vis.Network(container, data, options);
-
-    network.on("selectNode", function (params) {
-      console.log(params)
-      sendValue(params.nodes[0])
-    })
-
-    network.on("deselectNode", function (params) {
-      sendValue(null)
-    })
-
-
+    if (type == TYPE_NETWORK) {
+      renderNetwork(data, eventNames)
+    } else if (type == TYPE_TIMELINE) {
+      renderTimeline(data, eventNames)
+    } else if (type == TYPE_GRAPH3d) {
+      renderGraph3d(data, eventNames)
+    }
 
     // You'll most likely want to pass some data back to Python like this
     // sendValue({output1: "foo", output2: "bar"})
